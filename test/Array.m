01d2d889b5e885fa7cb60ed40f0fa26d529677f3
Wito <wito@eyego.no>
2010-05-19 09:20:23 +0200

    More thorough tests

--- test/Array.m
+++ test/Array.m
@@ -3,34 +3,50 @@
 
 int CGIKitTest_Array () {
 
+  // CGIArray
+  
   CGIAutoreleasePool *pool = [[CGIAutoreleasePool alloc] init];
   
   CGIArray *anArray = [[CGIArray alloc] initWithObjects:@"Peter", @"Frank", @"Tom", nil];
-  printf("%s\n", [[anArray description] UTF8String]);
+  printf("%s, %ld\n", [[anArray description] UTF8String], [anArray count]);
   CGIArray *anotherArray = [[CGIArray alloc] initWithObjects:@"Pan", [anArray autorelease], nil];
-  printf("%s\n", [[anotherArray description] UTF8String]);
+  printf("%s, %ld\n", [[anotherArray description] UTF8String], [anArray count]);
   [anotherArray release];
   
+  [CGIArray arrayWithObject:@"Boom!"];
+  printf("%d\n", [[[[CGIArray alloc] initWithObject:@"Bang!"] autorelease] indexOfObjectIdenticalTo:@"Bang!"]);
+  
+  id fakeArray[] = { @"Voom!", @"Wham!" };
+  [[[CGIArray alloc] initWithObjects:fakeArray count:2] autorelease];
+  
+  // CGIMutableArray
+  
   CGIMutableArray *yana = [[CGIMutableArray alloc] initWithObject:@"Alice"];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
   [yana addObject:@"Bob"];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
   [yana addObject:@"Carl"];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
   [yana addObject:@"Diane"];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
   [yana addObject:@"Elizabeth \xC2\xA9"];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
   [yana addObject:@"Fred"];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
   [yana insertObject:@"Gregory" atIndex:2];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
   [yana addObject:@"Hannelore"];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
   [yana addObject:@"Ida"];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
   [yana removeObjectAtIndex:1];
-  printf("%s\n", [[yana description] UTF8String]);
+  printf("%s, %ld\n", [[yana description] UTF8String], [yana count]);
+  [yana release];
+  
+  yana = [CGIMutableArray arrayWithObject:@"Shoop!"];
+  yana = [[[CGIMutableArray alloc] initWithObjects:fakeArray count:2] autorelease];
+  yana = [[[CGIMutableArray alloc] initWithObject:@"Woop!"] autorelease];
+  yana = [[[CGIMutableArray alloc] initWithObjects:@"Doom", @"Death", nil] autorelease];
   
   [pool release];
   
