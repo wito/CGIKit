f37d16dd1838b5179da6f11983097e6f35d1075a
Williham Totland <wito@eyego.no>
2010-05-26 17:28:10 +0200

    Actuate CGICoder test

--- test/harness.m
+++ test/harness.m
@@ -1,10 +1,12 @@
 #import "CGIKit/CGIKit.h"
 #import "stdio.h"
+#import <stdlib.h>
 
 int main () {
   @try {
     CGIKitTest_Array();
     CGIKitTest_String();
+    CGIKitTest_Archiver();
   }
   @catch (id e) {
     fprintf(stderr, "Terminating due to uncaught exception: %@\n", e);
