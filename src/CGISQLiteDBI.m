eba0dc84665eb20771f62dd73dcd6e66271ae450
Williham Totland <wito@eyego.no>
2010-06-22 21:50:18 +0200

    SQLite

--- src/CGISQLiteDBI.m
+++ src/CGISQLiteDBI.m
@@ -1,6 +1,8 @@
 #import "CGIKit/CGIDBI.h"
 #import "CGIKit/CGIString.h"
 #import "CGIKit/CGIArray.h"
+#import "CGIKit/CGIData.h"
+#import "CGIKit/CGIDictionary.h"
 
 #include <sqlite3.h>
 
@@ -32,11 +34,57 @@
   handle = NULL;
 }
 
-- (CGIUInteger)doQuery:(CGIDictionary *)query modalDelegate:(id)delegate {
+- (CGIUInteger)doQuery:(CGIDictionary *)query modalDelegate:(id <CGIDBIQueryDelegate>)delegate {
+  CGIString *sql = [query objectForKey:@"CGI_DBI_SQL_SENTENCE"];
+  const char *zSql = [sql UTF8String];
+  CGIArray *data = [query objectForKey:@"CGI_DBI_SQL_DATA"];
+  CGIUInteger parameterCount = [data count];
+  
+  sqlite3_stmt *statement;
+  sqlite3_prepare_v2(handle, zSql, -1, &statement, NULL);
+  
+  CGIUInteger i;
+  for (i = 1; i <= parameterCount; i++) {
+    id parameter = [data objectAtIndex:i - 1];
+    
+    if ([parameter isKindOfClass:[CGIString self]]) {
+      sqlite3_bind_text(statement, i, [parameter UTF8String], -1, SQLITE_STATIC);
+    } else if ([parameter isKindOfClass:[CGIData self]]) {
+      sqlite3_bind_blob(statement, i, [parameter bytes], [parameter length], SQLITE_STATIC);
+    }
+  }
+  
+  CGIUInteger retcol = sqlite3_column_count(statement);
   
   
+  CGIUInteger j;
+  i = 0;
+  while (sqlite3_step(statement) == SQLITE_ROW) {
+    CGIMutableArray *row = [[[CGIMutableArray alloc] init] autorelease];
+    i++;
+    for (j = 0; j < retcol; j++) {
+      // deal with column
+      
+      CGIUInteger type = sqlite3_column_type(statement, j);
+      
+      if (type == SQLITE_BLOB) {
+        CGIUInteger blobsize = sqlite3_column_bytes(statement, j);
+        const unsigned char *blob = sqlite3_column_blob(statement, j);
+        CGIData *blobData = [[[CGIData alloc] initWithBytes:blob length:blobsize] autorelease];
+        [row addObject:blobData];
+      } else {
+        const unichar *str = sqlite3_column_text(statement, j);
+        CGIString *string = [CGIString stringWithUTF8String:str];
+        [row addObject:string];
+      }
+    }
+    
+    [delegate DBI:self didGetRow:row];
+  }
   
-  return 0;
+  sqlite3_finalize(statement);
+  
+  return i;
 }
 
 
