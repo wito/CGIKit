b5be0b0f7bd341cf4a0777d201dcf3a6577dabe5
Williham Totland <wito@eyego.no>
2010-06-25 17:35:53 +0200

    -[search:table:properties]

--- CGIKit/CGIDBI.h
+++ CGIKit/CGIDBI.h
@@ -15,4 +15,7 @@
 
 - (CGIUInteger)doQuery:(CGIDictionary *)query modalDelegate:(id<CGIDBIQueryDelegate>)delegate;
 
+- (CGIUInteger)search:(CGIDictionary *)query table:(CGIString *)table modalDelegate:(id<CGIDBIQueryDelegate>)delegate;
+- (CGIUInteger)search:(CGIDictionary *)query table:(CGIString *)table properties:(CGIDictionary *)properties modalDelegate:(id<CGIDBIQueryDelegate>)delegate;
+
 @end
